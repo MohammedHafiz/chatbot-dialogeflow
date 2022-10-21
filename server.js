37e4ebfb1cd4aad7fbb10b1bd4d0a36891c13771
MohammedHafiz <mohammedhafizba@gmail.com>
2022-10-21 16:57:04 +0530

    test

--- server.js
+++ server.js
@@ -187,16 +187,16 @@ app.post("/webhook", async (req, res) => {
             // The math module contains a function
             // named toRadians which converts from
             // degrees to radians.
-            lon1 = parseInt(lon1) * Math.PI / 180;
-            lon2 = parseInt(lon2) * Math.PI / 180;
-            lat1 = parseInt(lat1) * Math.PI / 180;
-            lat2 = parseInt(lat2) * Math.PI / 180;
+            lon1 = lon1 * Math.PI / 180;
+            lon2 = lon2 * Math.PI / 180;
+            lat1 = lat1 * Math.PI / 180;
+            lat2 = lat2 * Math.PI / 180;
 
             console.log("lat1: " + lat1 + " lon1: " + lon1)
             console.log("lat2: " + lat2 + " lon2: " + lon2)
             // Haversine formula
-            let dlon = parseInt(lon2) - parseInt(lon1);
-            let dlat = parseInt(lat2) - parseInt(lat1);
+            let dlon = (lon2) - (lon1);
+            let dlat = (lat2) - (lat1);
             let a = Math.pow(Math.sin(dlat / 2), 2)
               + Math.cos(lat1) * Math.cos(lat2)
               * Math.pow(Math.sin(dlon / 2), 2);
