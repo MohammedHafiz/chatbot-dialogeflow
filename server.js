7e6353ca63a11275e81b03c62d4f77ba96db1d37
MohammedHafiz <mohammedhafizba@gmail.com>
2022-10-21 09:55:36 +0530

    testing

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.post("/webhook", async (req, res) => {
           resultQuery = await chatbot.textQuery(msg_body, from);
           console.log('message ::::::::::::::::::::::', resultQuery.fulfillmentText)
           // SendTextMessage(from, phone_number_id, resultQuery.fulfillmentText)
-          SendListMessage(phone_number, phone_number_id, "This is a list demo",
+          SendListMessage(from, phone_number_id, "This is a list demo",
             [{
               id: "1",
               title: resultQuery.fulfillmentText
